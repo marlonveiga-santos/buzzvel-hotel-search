195a6aa22a9485d8139f4589b99b7f6ec2112e16
Marlon Veiga <marlon_veiga@hotmail.com>
2021-10-15 02:08:52 -0300

    Add view provider and search page.

--- package/marveiga/hotel_search_package/src/HotelSearchPackageServiceProvider.php
+++ package/marveiga/hotel_search_package/src/HotelSearchPackageServiceProvider.php
@@ -9,6 +9,7 @@ class HotelSearchPackageServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/views', 'search');
     }
 
     public function register()

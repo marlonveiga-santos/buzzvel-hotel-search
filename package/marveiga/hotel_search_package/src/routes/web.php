39c6a87a29ac5cfb71bfca6013a0e4372a6eeedb
Marlon Veiga <marlon_veiga@hotmail.com>
2021-10-15 02:34:52 -0300

    create form for visual coordinates input.

--- package/marveiga/hotel_search_package/src/routes/web.php
+++ package/marveiga/hotel_search_package/src/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('search', function(){
     return view('search::search');
+})->name('search');
+
+Route::post('search', function(Request $request){
+    return $request->all();
 });
+
--- package/marveiga/hotel_search_package/src/views/search.blade.php
+++ package/marveiga/hotel_search_package/src/views/search.blade.php
@@ -9,5 +9,11 @@
 <body>
     <h1>Hotel Search</h1>
     <h2>Put your coordinates and click in search button.</h2>
+    <form action="{{route('search')}}" method="post">
+        @csrf
+        <label for="coord">Enter your coordinates (separated by comma)</label>
+        <input type="text" name="coordinates" id="coord" placeholder="enter your coordinates">
+        <input type="submit" value="Search">
+    </form>
 </body>
 </html>

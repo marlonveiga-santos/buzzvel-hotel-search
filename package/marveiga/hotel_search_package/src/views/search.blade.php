efb78d82b5f52a87656146e303c74a5e625364f5
Marlon Veiga <marlon_veiga@hotmail.com>
2021-10-15 16:11:04 -0300

    Add criteria option in search blade page.

--- package/marveiga/hotel_search_package/src/views/search.blade.php
+++ package/marveiga/hotel_search_package/src/views/search.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Hotel Search</title>
 </head>
+
 <body>
     <h1>Hotel Search</h1>
     <h2>Put your coordinates and click in search button.</h2>
@@ -13,8 +15,14 @@
         @csrf
         <label for="coord">Enter your coordinates (separated by comma)</label>
         <input type="text" name="coordinates" id="coord" placeholder="enter your coordinates">
+        <label for="criteria">Search criteria:</label>
+        <select name="seach_criteria" id="criteria">
+            <option value=“proximity”>Proximity</option>
+            <option value=“pricepernight”>Price per night</option>
+        </select>
         <input type="submit" value="Search">
     </form>
     {{$fool}}
 </body>
+
 </html>

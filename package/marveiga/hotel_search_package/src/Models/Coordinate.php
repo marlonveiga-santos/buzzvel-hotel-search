bb58e5dc9dcdaeabee4cacada55d0d86084552ea
Marlon Veiga <marlon_veiga@hotmail.com>
2021-10-15 04:26:09 -0300

    Create Coordinate Model.

---
+++ package/marveiga/hotel_search_package/src/Models/Coordinate.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace marveiga\HotelSearchPackage\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coordinate extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}

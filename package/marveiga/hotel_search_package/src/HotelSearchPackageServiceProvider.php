107205406fc9ddb84e0555956b36f3d4fa7ac4f1
Marlon Veiga <marlon_veiga@hotmail.com>
2021-10-15 21:41:56 -0300

    Prepare environment for package deploy.

--- package/marveiga/hotel_search_package/src/HotelSearchPackageServiceProvider.php
+++ package/marveiga/hotel_search_package/src/HotelSearchPackageServiceProvider.php
@@ -10,6 +10,9 @@ class HotelSearchPackageServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/routes/web.php');
         $this->loadViewsFrom(__DIR__.'/views', 'search');
+        $this->publishes([
+            __DIR__ . '/views' => resource_path('views/vendor/hotel_search_package')
+        ]);
     }
 
     public function register()

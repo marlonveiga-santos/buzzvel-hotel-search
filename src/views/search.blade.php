88da6846dccc9ed9b83a3cffc3a735c0d5356b52
Marlon Veiga <marlon_veiga@hotmail.com>
2021-10-16 17:34:05 -0300

    Add input pattern matching in search view.

--- src/views/search.blade.php
+++ src/views/search.blade.php
@@ -14,7 +14,7 @@
     <form action="{{route('search')}}" method="post">
         @csrf
         <label for="coord">Enter your coordinates (separated by comma)</label>
-        <input type="text" name="coordinates" id="coord" placeholder="enter your coordinates" required="required">
+        <input type="text" name="coordinates" id="coord" placeholder="enter your coordinates" required="required" pattern="[0-9,-. ]+">
         <label for="criteria">Search criteria:</label>
         <select name="search_criteria" id="criteria">
             <option value=“proximity”>Proximity</option>
